91eac7d53eb1cb88ed305eb51837838a8abb954b
julienleonard <jl.julien.leonard@gmail.com>
2015-07-22 20:26:27 +1000

    Refactor into smaller functions

--- myflickr.py
+++ myflickr.py
@@ -9,6 +9,68 @@ import sys
 from basics import *
 import xml.etree.ElementTree
 
+
+def flickr_photo_stats(flickr,api_key,photo):
+    # get the title
+    phototitle = photo.attrib['title']
+
+    # get timestamp of the photo
+    photoinfo      = flickr.photos_getInfo(api_key=api_key,photo_id=photo.attrib['id'])
+    photoxml       = list(photoinfo.iter("photo"))[0]
+    dates          = list(photoxml.iter("dates"))[0]
+    phototimestamp = dates.attrib['posted']
+            
+    # get the list of favorites for the photo
+    time.sleep(1)
+    favs       = flickr.photos_getFavorites(api_key=api_key,photo_id=photo.attrib['id'])
+    favxml     = list(favs.iter("photo"))[0]
+    favcount   = favxml.attrib['total']
+    personlist = list(favxml.iter("person"))
+    favedates  = [person.attrib['favedate'] for person in personlist]
+
+    return (phototitle, phototimestamp, favcount, favedates)
+
+
+def flickr_photos_stats(flickr,api_key,user_id):
+    result = []
+    perpage = 10
+    pageindex = 1
+    rsp = flickr.people_getPhotos(api_key=api_key,user_id=user_id,per_page=perpage,page=pageindex)
+    photoss = list(rsp.iter("photos"))[0];
+
+    while int(photoss.attrib['page']) < int(photoss.attrib['pages']):
+        puts("page index",pageindex)
+        time.sleep(10)
+        photolist = list(photoss.iter("photo"));
+        photoindex = 0
+        for photo in photolist:
+            time.sleep(1)
+
+            # get photo stats and add data to cache structure
+            result.append(flickr_photo_stats(flickr,api_key,photo))
+
+            # iter to the next photo
+            photoindex += 1
+            
+        # iter to the next page
+        pageindex += 1
+        rsp = flickr.people_getPhotos(api_key=api_key,user_id="22283623@N00",per_page=perpage,page=pageindex)
+        photoss = list(rsp.iter("photos"))[0];
+
+    return result
+
+
+def flickr_xml_photosstats(photosstats,timestamp):
+    result = "<flickr timestamp=\"" + str(timestamp) + "\">\n"
+    for photostats in photosstats:
+        (title,timestamp,total,favdates) = photostats
+        result += "   <photo title=\"" + title + "\" \t timestamp=\"" + timestamp + "\" \t count=\"" + total + "\" >\n"
+        for favdate in favedates:
+            result += "      <favedate timestamp=\"" + favdate + "\"/>\n"
+        result += "   </photo>\n"
+    result += "</flickr>\n"
+    return result
+
 #
 # method to dump social stats about the flickr user account
 # args:
@@ -29,68 +91,20 @@ def flickr_dump(api_secret,api_key,user_id,filepath):
     flickr.get_token_part_two((token, frob))
 
     #
-    # get the photo data
+    # get the photos stats
     #
-    myphotos = []
-    perpage = 10
-    pageindex = 1
-    rsp = flickr.people_getPhotos(api_key=api_key,user_id=user_id,per_page=perpage,page=pageindex)
-    photoss = list(rsp.iter("photos"))[0];
-
-    while int(photoss.attrib['page']) < int(photoss.attrib['pages']):
-        puts("page index",pageindex)
-        time.sleep(10)
-        photolist = list(photoss.iter("photo"));
-        photoindex = 0
-        for photo in photolist:
-            time.sleep(1)
-
-            # get the title
-            phototitle = photo.attrib['title']
-
-            # get timestamp of the photo
-            photoinfo      = flickr.photos_getInfo(api_key=api_key,photo_id=photo.attrib['id'])
-            photoxml       = list(photoinfo.iter("photo"))[0]
-            dates          = list(photoxml.iter("dates"))[0]
-            phototimestamp = dates.attrib['posted']
-            
-            # get the list of favorites for the photo
-            time.sleep(1)
-            favs       = flickr.photos_getFavorites(api_key=api_key,photo_id=photo.attrib['id'])
-            favxml     = list(favs.iter("photo"))[0]
-            favcount   = favxml.attrib['total']
-            personlist = list(favxml.iter("person"))
-            favedates  = [person.attrib['favedate'] for person in personlist]
-
-            # add data to cache structure
-            myphotos.append((phototitle, phototimestamp, favcount, favedates))
-
-            # stdout info
-            puts("photo",photo.attrib['id'],phototitle,"favedates",favedates)
-
-            # iter to the next photo
-            photoindex += 1
-            
-        # iter to the next page
-        pageindex += 1
-        rsp = flickr.people_getPhotos(api_key=api_key,user_id="22283623@N00",per_page=perpage,page=pageindex)
-        photoss = list(rsp.iter("photos"))[0];
+    photosstats = flickr_photos_stats(flickr,api_key,user_id)
 
     #
     # format the data into xml
     #
-    result = "<flickr timestamp=\"" + str(time.time()) + "\">\n"
-    for photo in myphotos:
-        (title,timestamp,total,favdates) = photo
-        result += "   <photo title=\"" + title + "\" \t timestamp=\"" + timestamp + "\" \t count=\"" + total + "\" >\n"
-        for favdate in favedates:
-            result += "      <favedate timestamp=\"" + favdate + "\"/>\n"
-        result += "   </photo>\n"
-    result += "</flickr>\n"
+    xmlphotostats = flickr_xml_photosstats(photosstats,time.time())
 
+    #
     # dump the xml result in a file
+    #
     output=open(filepath, 'w+')
-    output.write(result.encode('utf8'))
+    output.write(xmlphotostats.encode('utf8'))
     output.close()
 
 
