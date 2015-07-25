e5abf855fcdc6fe032bb5c71834c888904d85e7f
julienleonard <jl.julien.leonard@gmail.com>
2015-07-25 21:19:41 +1000

    corrections wordpress_stats

--- wordpress_stats.py
+++ wordpress_stats.py
@@ -7,7 +7,7 @@ import datetime
 # get all wordpress stats data
 #
 def wordpress_stats():
-    stats = []
+    result = []
     offset = 0
     baseurl = "https://public-api.wordpress.com/rest/v1/sites/" + blogid + "/posts?number=100&offset="
     while True:
@@ -20,18 +20,18 @@ def wordpress_stats():
             break
         offset += 100
         
-    return stats
+    return result
 
 #
 # format stats into xml
 #
 def wordpress_xmlstats(stats,timestamp):
-    content = "<wordpress nfollowers=\"" + "NA" + "\" timestamp=\"" + str(timestamp) + "\">\n"
+    xmlcontent = "<wordpress nfollowers=\"" + "NA" + "\" timestamp=\"" + str(timestamp) + "\">\n"
     for stat in stats:
         (title,data,nfavs) = stat
-        content = content + "\t<post name=\"" + title + "\" \t timestamp=\"" + str(date) + "\" \t fav_count=\"" + str(nfavs) + "\"></post>\n"
-    content = content + "</wordpress>\n"
-    return content
+        xmlcontent = xmlcontent + "\t<post name=\"" + title + "\" \t timestamp=\"" + str(date) + "\" \t fav_count=\"" + str(nfavs) + "\"></post>\n"
+    xmlcontent = xmlcontent + "</wordpress>\n"
+    return xmlcontent
 
 
 #
@@ -40,7 +40,7 @@ def wordpress_xmlstats(stats,timestamp):
 def wordpress_dump(blogid,xmloutputfilepath): 
 
     # get all the posts
-    posts = wordpress_postdata()
+    stats = wordpress_stats()
 
     # format the data 
     xmlcontent = wordpress_xmlstats(stats,time.time())
